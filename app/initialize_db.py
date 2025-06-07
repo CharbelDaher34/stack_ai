a7dbaed73a1eaac25cacd58ffc4c41c6115ba4da
EuriskoMobility <root@s10932141.dedi.leaseweb.net>
2025-06-07 12:32:22 +0000

    .

--- app/initialize_db.py
+++ app/initialize_db.py
@@ -1,7 +1,2 @@
-from core.db import create_db_and_tables, create_sample_data
-
-def initialize_db():
-    create_db_and_tables()
-    create_sample_data(num_libraries=10, docs_per_library=10, chunks_per_doc=10)
-
+from core.db import initialize_db
 initialize_db()
